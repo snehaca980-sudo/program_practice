93c91b4bc027b63d8e6cb8170bc29ad041c762e4
snehaca980-sudo <snehaca980@gmail.com>
2026-09-15 11:35:08 +0530

    save pyp3.py

---
+++ pyp3.py
@@ -0,0 +1,16 @@
+
+# Program to swap two numbers
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+print("\nBefore swapping:")
+print("First number:", num1)
+print("Second number:", num2)
+
+# Swapping the values
+num1, num2 = num2, num1
+
+print("\nAfter swapping:")
+print("First number:", num1)
+print("Second number:", num2)
