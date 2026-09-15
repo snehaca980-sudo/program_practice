dc0b647db27ecee93e98ac1ab54034fa9714cd5e
snehaca980-sudo <snehaca980@gmail.com>
2026-09-15 11:58:05 +0530

    Added addition and subtraction

---
+++ calculator.py
@@ -0,0 +1,11 @@
+
+# Program to perform addition and subtraction
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+addition = num1 + num2
+subtraction = num1 - num2
+
+print("Addition:", addition)
+print("Subtraction:", subtraction)
